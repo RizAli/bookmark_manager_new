913ab9245a2db74eb9add3407450ec4e4c093be7
Rizwan Ali <rizcube@gmail.com>
2015-02-26 11:31:07 +0000

    create folder structure, Added two databases test, development

---
+++ lib/server.rb
@@ -0,0 +1,13 @@
+require 'data_mapper'
+
+env = ENV['RACK_ENV'] || 'development'
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_new#{env}")
+
+require './app/models/link'
+
+
+DataMapper.finalize
+
+
+DataMapper.auto_upgrade!
